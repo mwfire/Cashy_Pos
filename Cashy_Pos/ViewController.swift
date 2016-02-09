c6922e8c3f005c034447638e6e9609097326bdec
Jhoan <jhoannarango@msn.com>
2016-02-09 01:28:12 -0500

    Initial Commit

---
+++ Cashy_Pos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cashy_Pos
+//
+//  Created by Jhoan Arango on 2/9/16.
+//  Copyright © 2016 Jhoan Arango. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
